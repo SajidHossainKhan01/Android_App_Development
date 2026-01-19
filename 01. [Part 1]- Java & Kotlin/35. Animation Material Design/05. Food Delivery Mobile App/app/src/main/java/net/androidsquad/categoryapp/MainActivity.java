bc91e7ac46d74432b1c27b3c237f277c51e71f84
SajidHossainKhan01 <sk6819152@gmail.com>
2026-01-19 19:04:01 +0600

    Upload Food Delivery Mobile App

---
+++ 01. [Part 1]- Java & Kotlin/35. Animation Material Design/05. Food Delivery Mobile App/app/src/main/java/net/androidsquad/categoryapp/MainActivity.java
@@ -0,0 +1,17 @@
+package net.androidsquad.categoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
